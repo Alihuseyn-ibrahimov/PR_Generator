cce4b60d5874abc50991ffe3bc033fbbd49a97a1
Alihuseyn <ibrahimov.alihuseyn@gmail.com>
2026-06-14 00:23:22 +0400

    Update pr.py

--- pr.py
+++ pr.py
@@ -16,7 +16,7 @@ try:
     # Buluddakı Secrets bölməsindən açarı çəkirik
     GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
     genai.configure(api_key=GOOGLE_API_KEY)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-1.0-pro')
     api_status = True
 except Exception:
     st.warning(
